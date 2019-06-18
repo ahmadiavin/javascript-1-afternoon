1d87a10bab28496ad6390bd23b243b028a39eedc
Ali_Ahmadiavin <ahmadiavin@gmail.com>
2019-06-18 14:22:19 -0500

    initial commit

--- practice.js
+++ practice.js
@@ -2,31 +2,31 @@
 
 //Create a variable called myName that is a string data type
 
-//Code here
+let myName = "Ali";
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-//Code here
+let myAge = 31;
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-//Code here
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-//Code here
+let greatestFear = undefined 
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-//Code here
+let devMountainGoal = null
 
 //////////////////PROBLEM 6////////////////////
 
@@ -35,21 +35,30 @@
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+function greeting(myName){ 
+  return ("Hello, "  + myName);
+
+}
+
+greeting(myName);
 
 //////////////////PROBLEM 7////////////////////
 
 //Rewrite the function greeting as a function expression.
 //Name it newGreeting.
 
-//Code Here
+let newGreeting = function(myName){
+  return ("Hello, "  + myName);
+
+}
+newGreeting(myName);
 
 //////////////////PROBLEM 8////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+let groceries = ["apples", "milk", "eggs","bread"];
 
 //////////////////PROBLEM 9////////////////////
 
@@ -58,12 +67,17 @@
 //name (a string), color (a string), age (a number),
 //and goodBoy (a boolean).
 
-//Code Here
+let dog = function(dog){
+  name: Jub;
+  color: green;
+  age: 6;
+  goodBoy: true; 
+}
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
-//Code Here
+
 
 //////////////////PROBLEM 10////////////////////
 
